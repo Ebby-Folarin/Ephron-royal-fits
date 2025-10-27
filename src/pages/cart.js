c18d0daa010128410bedffe507bfff58ec2caa0b
Ebby-Folarin <ebbyfolarin95@gmail.com>
2025-10-27 22:01:46 +0000

    Refactor CartPage meta tags and schema usage
    Updated meta tags and schema handling in CartPage.

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,49 +1,33 @@
 /* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @next/next/no-sync-scripts */
 import Head from "next/head";
 import Navbar from "@/components/navigation/navbar/navbar";
 import Footer from "@/components/navigation/footer/footer";
 import { getWSSchema, getWPSchema, getLBSchema } from "@/components/schema";
-
 import { useState } from "react";
 import SearchBox from "@/components/search/search";
 import Cart from "@/components/cart/cart";
 import Script from "next/script";
 
-
-
 export default function CartPage() {
   const [showSearch, setShowSearch] = useState(false);
   const handleShowSearch = (state) => setShowSearch(state);
 
-  // page default data
   const pageName = "Ephron Royal 'fits - Cart";
   const pageDesc =
-    "Elevate your style with Ephron Royal 'fits, where art meets fashion in a harmonious blend of creativity and elegance. Explore our online store for a curated collection of unique art-inspired fashion pieces that allow you to express your individuality. Immerse yourself in a world where every garment tells a story, bringing together the realms of art and fashion seamlessly. Discover the perfect fusion of artistic expression and sartorial sophistication at Ephron Royal 'fits.";
+    "Elevate your style with Ephron Royal 'fits, where art meets fashion in a harmonious blend of creativity and elegance. Explore our online store for a curated collection of unique art-inspired fashion pieces that allow you to express your individuality.";
   const pageKeywords =
-    "Art-inspired fashion, Unique fashion pieces, Creative clothing, Fashion with a story, Wearable art, Eccentric style, Artistic expression in fashion, Fashion showcase, Individuality in clothing, Ephron Royal 'fits online store";
+    "Art-inspired fashion, Unique fashion pieces, Creative clothing, Fashion with a story, Wearable art, Ephron Royal 'fits online store";
+
   const baseURL = "https://ephronroyalfits.com/";
-  const pageURL = "https://ephronroyalfits.com/cart";
+  const pageURL = `${baseURL}cart`;
 
-  // web site schema
   const wSSchema = getWSSchema(baseURL);
-
-  // web page schema
   const wPSchema = getWPSchema(pageName, pageDesc, baseURL, [
-    {
-      "@type": "ListItem",
-      position: 1,
-      name: "Home",
-      item: baseURL,
-    },
-    {
-      "@type": "ListItem",
-      position: 2,
-      name: pageName,
-      item: pageURL,
-    },
+    { "@type": "ListItem", position: 1, name: "Home", item: baseURL },
+    { "@type": "ListItem", position: 2, name: pageName, item: pageURL },
   ]);
 
-  // local business schema
   const lBSchema = getLBSchema(
     pageName,
     {
@@ -56,31 +40,24 @@ export default function CartPage() {
     "+234-706-386-9144",
     "ephronroyalfits@gmail.com",
     baseURL,
-    `${baseURL}/logo/png/logo.png`,
+    `${baseURL}logo/png/logo.png`,
     "Cash, Credit Card, Transfer",
     "NGN, USD, EURO",
     "Mo-Fr 09:00-17:00",
-    {
-      latitude: "7.6183612",
-      longitude: "5.1391947",
-    }
+    { latitude: "7.6183612", longitude: "5.1391947" }
   );
 
   return (
     <>
       <Head>
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{pageName}</title>
-
         <meta name="description" content={pageDesc} />
         <meta name="keywords" content={pageKeywords} />
         <meta name="theme-color" content="#ffffff" />
         <link rel="icon" type="image/x-icon" href="/logo/png/logo_trans.png" />
-        <meta name="author" content="Ephron Royal 'fits" />
+        <meta name="author" content="Ephron Royal fits" />
         <meta property="og:title" content={pageName} />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/logo/png/logo_text.png" />
@@ -92,24 +69,29 @@ export default function CartPage() {
 
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(wSSchema) }}
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(wSSchema || {}),
+          }}
         />
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(wPSchema) }}
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(wPSchema || {}),
+          }}
         />
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(lBSchema) }}
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(lBSchema || {}),
+          }}
         />
       </Head>
-      <Script src="https://checkout.flutterwave.com/v3.js" />
+
+      <Script src="https://checkout.flutterwave.com/v3.js" strategy="afterInteractive" />
 
       <main>
         <Navbar emitShowSearch={handleShowSearch} />
-
         <Cart />
-
         <Footer />
       </main>
 
